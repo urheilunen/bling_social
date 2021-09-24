d4f795d9f72530e3f0d8a80ea3e0066dd881e768
urheilunen <quarussique@yandex.ru>
2021-09-24 13:57:07 +0500

    Some css added. New functions: remove post (by author only), subscribing and unsubscribing. New bugs: UserProfile needs to be fixed because of some problems with reaching custom fields like subscribers, birth date, etc.

--- bling/urls.py
+++ bling/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.views import LoginView
 from django.urls import path, include
-from .views import index, user_profile, BlingPostCreateView
+from .views import index, user_profile, BlingPostCreateView, remove_post, subscribe, unsubscribe
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,6 +9,9 @@ urlpatterns = [
     path('user/<str:user_id>/', user_profile, name='user_profile'),
     path('newpost/', BlingPostCreateView.as_view(), name='newpost'),
     # path('newpost/', newpost, name='newpost'),
+    path('removepost/<int:post_id>/', remove_post, name='remove_post'),
+    path('subscribe/<str:subscribant_username>/', subscribe, name='subscribe'),
+    path('unsubscribe/<str:subscribant_username>/', unsubscribe, name='unsubscribe'),
     path('', index, name='home'),
 ]
 
--- bling/views.py
+++ bling/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseNotAllowed
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, DeleteView
 from .models import BlingPost, UserProfile
 from django.contrib.auth.models import User
+from django.urls import reverse
 from .forms import BlingPostForm
 
 
@@ -22,6 +24,14 @@ class BlingPostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
+# class BlingPostDeleteView(LoginRequiredMixin, DeleteView):
+#     model = BlingPost
+#     success_url = '/'
+#
+#     def get_context_data(self, *args, **kwargs):
+#         context = super().get_context_data(*args, **kwargs)
+#         return context
 
 def index(request):
     blingposts = BlingPost.objects.all()
@@ -49,11 +59,26 @@ def user_profile(request, user_id):
 
     blingposts = BlingPost.objects.filter(author=blinguser)  # posts of this user
 
+    # check if it is request.user's page or not so they cannot self-(un)subscribe
+    if request.user == blinguser:
+        # this page is request.user's own, must do nothing
+        is_own_page = True
+        is_current_user_subscribed = None
+    else:
+        # it's someone else's page, user can (un)subscribe
+        is_own_page = False
+        # check if current user is subscribed to this user to decide which button to render
+        if request.user in subs:
+            is_current_user_subscribed = True
+        else:
+            is_current_user_subscribed = False
+
     # check if friends and subs are more than 3, then they won't fit on the user page
     if friends_amount > 3:
         friends = friends[:3]
     if subs_amount > 3:
         subs = subs[:3]
+
     context = {
         'blinguser': blinguser,
         'blingposts': blingposts,
@@ -62,5 +87,51 @@ def user_profile(request, user_id):
         'friends_amount': friends_amount,
         'subs': subs,
         'subs_amount': subs_amount,
+        'is_own_page': is_own_page,
+        'is_current_user_subscribed': is_current_user_subscribed,
     }
     return render(request, 'bling/user_profile.html', context)
+
+
+def remove_post(request, post_id):
+    try:
+        # searching for the post with this primary key
+        post = BlingPost.objects.get(pk=int(post_id))
+        # getting the author of the post to approve that the very author is deleting their creation
+        post_author = post.author
+        if request.user == post_author:
+            # if deleting is requested by the author, then it's ok
+            post.delete()
+            # redirecting to the profile of author
+            # because we are already there, user won't see the difference, post will just vanish in a moment
+            new_url = '/user/' + post_author.username + '/'
+            return redirect(new_url)
+        else:
+            # deleting is not requested by author. Go away, criminal scum
+            return HttpResponseNotAllowed("<h2>Not allowed</h2>")
+    except BlingPost.DoesNotExist:
+        # no such post, wtf
+        return HttpResponseNotFound("<h2>Post not found</h2>")
+
+
+def subscribe(request, subscribant_username):
+    try:
+        subscribant_user = User.objects.get(username=subscribant_username)
+        subscribant_userprofile = UserProfile.objects.get(user=subscribant_user)
+        subscribant_userprofile.subscribers.add(request.user)
+        new_url = '/user/' + subscribant_username + '/'
+        return redirect(new_url)
+    except User.DoesNotExist:
+        return HttpResponseNotFound("<h2>User not found</h2>")
+
+
+def unsubscribe(request, subscribant_username):
+    try:
+        subscribant = User.objects.get(username=subscribant_username)
+        # custom subscribant-user fields (born_on, friends, etc.)
+        custom_subscribant = UserProfile.objects.get(user=User.objects.get(username=subscribant_username))
+        custom_subscribant.subscribers.remove(request.user)
+        new_url = '/user/' + subscribant_username + '/'
+        return redirect(new_url)
+    except User.DoesNotExist:
+        return HttpResponseNotFound("<h2>User not found</h2>")
