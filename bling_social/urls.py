38e0bb36ddaece3909545061274a4de815afe373
urheilunen <quarussique@yandex.ru>
2021-09-27 17:54:47 +0500

    New signin and signup pages style.

--- bling_social/urls.py
+++ bling_social/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('', include('bling.urls')),
 ]
