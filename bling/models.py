408898a280f25c4a31689aab79042a954dae546f
urheilunen <quarussique@yandex.ru>
2021-10-10 21:20:05 +0500

    Removed un/subscription functions from separate to common view, because it is not a link, it is a button (in form) now.

--- bling/models.py
+++ bling/models.py
@@ -94,45 +94,7 @@ class BlingComment(models.Model):
         return self.text
 
     def how_old(self):
-        # generating the list of each unit of comment's age
-        date_list = [
-            datetime.datetime.now(datetime.timezone.utc).year - self.created_on.year,
-            datetime.datetime.now(datetime.timezone.utc).month - self.created_on.month,
-            datetime.datetime.now(datetime.timezone.utc).day - self.created_on.day,
-            datetime.datetime.now(datetime.timezone.utc).hour - self.created_on.hour,
-            datetime.datetime.now(datetime.timezone.utc).minute - self.created_on.minute,
-            datetime.datetime.now(datetime.timezone.utc).second - self.created_on.second
-        ]
-        declension_list = [
-            ['год', 'года', 'лет'],
-            ['месяц', 'месяца', 'месяцев'],
-            ['день', 'дня', 'дней'],
-            ['час', 'часа', 'часов'],
-            ['минуту', 'минуты', 'минут'],
-            ['секунду', 'секунды', 'секунд']
-        ]
-
-        def get_declension(num):
-            if 5 <= num <= 19:
-                return 2
-            elif num % 10 == 1:
-                return 0
-            elif 2 <= (num % 10) <= 4:
-                return 1
-            else:
-                return 2
-
-        age = ''
-
-        for i in range(6):
-            if date_list[i] == 0:
-                pass
-            else:
-                # describe all russian words to every kind of number and datetime attribute name
-                age = str(date_list[i]) + ' ' + declension_list[i][get_declension(date_list[i])]
-                break
-
-        return age
+        return how_old_is_this_datetime(self.created_on)
 
 
 class BlingImage(models.Model):
